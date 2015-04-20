f774dc4856a0c903b415846412e24c8d7cec6e8e
Gilles Degols <gdegols224@gmail.com>
2015-04-20 20:56:43 +0200

    Multi-jobs version of hbase_import.py

--- hbase-benchmarks/hbase_import_process.py
+++ hbase-benchmarks/hbase_import_process.py
@@ -1,3 +1,4 @@
+ 
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
@@ -19,6 +20,7 @@ import subprocess
 import threading
 import MySQLdb # See http://stackoverflow.com/questions/372885/how-do-i-connect-to-a-mysql-database-in-python
 import MySQLdb.cursors
+from multiprocessing import Process, Manager
 
 # This script will download the data from the highlander database, create a json from it, then upload it to 
 # the cgs system, which is, for now, constituted of a hbase database where it will save the data.
@@ -27,20 +29,21 @@ import MySQLdb.cursors
 
 # Configuration for the user
 highlander_host = "highlander.usr.hydra.vub.ac.be"
+highlander_host = "172.31.244.166"
 highlander_database = "Iridia"
-highlander_user = "iridia"
-highlander_password = "iri.2742"
+highlander_user = ""
+highlander_password = ""
 
 local_host = "127.0.0.1"
 local_database = "highlander_chromosomes"
-local_user = "root"
-local_password = "Olgfe65grgr"
+local_user = ""
+local_password = ""
 
-current_server_url = 'http://62.210.254.52'
+current_server_url = ''
 
-cluster_url = 'http://insilicodb.ulb.ac.be:8888'
+cluster_url = ''
 querySession = requests.Session()
-info = {'username':'gdegols','password':'z9FNeTrQJYaemAtyUVva'}
+info = {'username':'','password':''}
 r = querySession.post(cluster_url+'/accounts/login/',data=info)
 
 target_database = "hbase" # "hbase" or "impala_text"
@@ -257,53 +260,65 @@ def saveForLater(cur, patient, benchmark_table):
     
     return True
 
-class patientToTSV(threading.Thread):
+def launchPatientToTSV(patient, benchmark_table, procnum, return_dict):
+    t = patientToTSV()
+    t.setBenchmarkTable(benchmark_table)
+    t.setPatient(patient)
+    
+    res = t.launch()
+    
+    return_dict[procnum] = res
+    
+class patientToTSV():
     m_patient = None
-    m_f = None
     m_benchmark_table = None
     
     def setPatient(self, patient):
         self.m_patient = patient
-            
-    def setF(self, f):
-        self.m_f = f
-        
+                    
     def setBenchmarkTable(self, benchmark_table):
         self.m_benchmark_table = benchmark_table
         
-    def run(self):
+    def launch(self):
         patient = self.m_patient
-        f = self.m_f
         benchmark_table = self.m_benchmark_table
         
-        st = time.time()
-        connexion = MySQLdb.connect(host= highlander_host, user=highlander_user, passwd=highlander_password,db=highlander_database, cursorclass=MySQLdb.cursors.DictCursor, compress=False)
-        cur = connexion.cursor()
-        
-        t = patient+": Downloading data. "
+        attempts = 0
+        connexion = None
+        while connexion is None:
+            try:
+                connexion = MySQLdb.connect(host= highlander_host, user=highlander_user, passwd=highlander_password,db=highlander_database, cursorclass=MySQLdb.cursors.DictCursor, compress=False)
+                cur = connexion.cursor()
+            except:
+                print(patient+": Failed to connect.")
+                time.sleep(1.0)
+                connexion = None
+                attempts += 1
+                if attempts > 3:
+                    print(patient+": abort connexion.")
+                    return ''
+            
+        t = patient+": Downloading data. "        
         st = time.time()
         cur.execute("SELECT * FROM "+benchmark_table+" WHERE patient = '"+patient+"' ORDER BY id") # 15s
         print(t+ "Time: "+str(round(time.time()-st,2))+"s.")
         
         if cur.rowcount < 40000:
             print(patient+": Probably incomplete data found (rows = "+str(cur.rowcount)+" < 40 000), we stop here.")
-            return False
-        data = cur.fetchall()
-        cur.close()
-        
+            return ''        
+            
         # We convert the variants to a tsv text        
         t = patient+": Converting data ("+str(cur.rowcount)+" lines). "
         st = time.time()
-        variants = totsvForBenchmarks(data, patient)        
+        variants = totsvForBenchmarks(cur.fetchall(), patient)        
         print(t+"Time: "+str(round(time.time()-st,2))+"s.")
-                
-        # We save the file to the current web server
-        print(patient+": Saving (compressed) tsv data. "),
-        st = time.time()    
-        f.write(variants)           
-        print("Time: "+str(round(time.time()-st,2))+"s.")
         
+        cur.close()
         connexion.close()
+        
+        # We return the text
+        return variants
+        
         
 # This function should be only use for benchmarks purposes as we don't use json anymore
 def saveToTSV(cur, sample, last_sample, benchmark_table):
@@ -315,60 +330,57 @@ def saveToTSV(cur, sample, last_sample, benchmark_table):
     patient = 'NA'+(str(sample).zfill(5))
     f = gzip.open(server_directory+'/hbase_upload_'+str(patient)+'_'+benchmark_table+'.tsv.gz', 'wb')
     
-    threads = []    
-    max_threads = 4
+    processes = []    
+    max_processes = 5
     st_init = time.time()
+    manager = Manager()
+    return_dict = manager.dict()
     for sample in xrange(sample, last_sample):    
         patient = 'NA'+(str(sample).zfill(5))
         
-        if True:
-            if len(threads) == 0:
-                st_init = time.time()
-                
-            t = patientToTSV()
-            t.setDaemon(True)
-            t.setF(f)
-            t.setBenchmarkTable(benchmark_table)
-            t.setPatient(patient)
+        if len(processes) == 0:
+            st_init = time.time()
+            manager = Manager()
+            return_dict = manager.dict()
             
-            t.start()
-            threads.append(t)
+        d = Process(name='launchPatientToTSV', target=launchPatientToTSV, args=(patient, benchmark_table, len(processes), return_dict))
+        d.daemon = True            
+        d.start()
+        processes.append(d)
+        
+        if len(processes) >= max_processes:
+            for d in processes:
+                d.join()
             
-            if len(threads) >= max_threads:
-                for t in threads:
-                    t.join()
-                print(str(max_threads)+" samples done in "+str(time.time()-st_init)+"s")
-                st_init = time.time()
-                threads = []
-        else:
-            # We download the variants for the given patient
-            print(patient+": Downloading data."),
-            st = time.time()
-            cur.execute("SELECT * FROM "+benchmark_table+" WHERE patient = '"+patient+"' ORDER BY id") # 15s
-            print("Time: "+str(round(time.time()-st,2))+"s.")
-            
-            if cur.rowcount < 40000:
-                print(patient+": Probably incomplete data found (rows = "+str(cur.rowcount)+" < 40 000), we stop here.")
-                return False
-            
-            # We convert the variants to a tsv text
-            print(patient+": Converting data ("+str(cur.rowcount)+" lines)."),
-            st = time.time()
-            variants = totsvForBenchmarks(cur.fetchall(), patient)        
-            print("Time: "+str(round(time.time()-st,2))+"s.")
-               
             # We save the file to the current web server
-            print(patient+": Saving (compressed) tsv data. "),
+            print("Saving (compressed) tsv data for the different samples. "),
             st = time.time()    
-            f.write(variants)           
+            for res in xrange(0, len(processes)):
+                f.write(return_dict[res])           
             print("Time: "+str(round(time.time()-st,2))+"s.")
         
-    if len(threads) >= 1:
-        for t in threads:
+            print(str(max_processes)+" samples done in "+str(round(time.time()-st_init,2))+"s")
+            st_init = time.time()
+            processes = []
+            manager = Manager()
+            return_dict = manager.dict()        
+        
+    if len(processes) >= 1:
+        for t in processes:
             t.join()
-        print(str(max_threads)+" samples done in "+str(time.time()-st_init)+"s")
+            
+        # We save the file to the current web server
+        print("Saving (compressed) tsv data for the different samples. "),
+        st = time.time()    
+        for res in xrange(0, len(processes)):
+            f.write(return_dict[res])           
+        print("Time: "+str(round(time.time()-st,2))+"s.")
+    
+        print(str(max_processes)+" samples done in "+str(round(time.time()-st_init,2))+"s")
         st_init = time.time()
-        threads = []
+        processes = []
+        manager = Manager()
+        return_dict = manager.dict()
         
     f.close() 
     
@@ -468,56 +480,54 @@ def testIfCompressWorthIt(variants):
     
     return True
     
-# We connect to the db
-highlander_connexion = MySQLdb.connect(host= highlander_host, user=highlander_user, passwd=highlander_password,db=highlander_database, cursorclass=MySQLdb.cursors.DictCursor, compress=False)
-# sudo ip add add dev tun0 172.31.236.177/24 broadcast 172.31.236.178
-# We count the data available in each analysis
-analyses = [('small', 200, '20_2015_04_01_benchmarks_small'), ('medium', 1000,'21_2015_04_01_benchmarks_medium')]#,('big',5000,'22_2015_04_01_benchmarks_big'),('huge',25000,'23_2015_04_01_benchmarks_huge')]
-starting_sample = 100
-for analysis in analyses:
-    cur = highlander_connexion.cursor()
-    # Not a good idead to use the following query as the table is huge...
-    #cur.execute("SELECT DISTINCT(patient) FROM "+analysis[0])    
-    #samples = cur.fetchall()
-    
-    # For each sample we will download the data, then create a json from it, and upload it to hbase
-    if global_upload_state is False:
-        increment = 1000
-    else:
-        increment = 1
+if __name__ == '__main__':
+    # We connect to the db
+    #highlander_connexion = MySQLdb.connect(host= highlander_host, user=highlander_user, passwd=highlander_password,db=highlander_database, cursorclass=MySQLdb.cursors.DictCursor, compress=False)
+    cur = 0
+    # sudo ip add add dev tun0 172.31.236.177/24 broadcast 172.31.236.178
+    # We count the data available in each analysis
+    analyses = [('small', 200, '20_2015_04_01_benchmarks_small'), ('medium', 1000,'21_2015_04_01_benchmarks_medium'),('big',5000,'22_2015_04_01_benchmarks_big')]#,('huge',25000,'23_2015_04_01_benchmarks_huge')]
+    starting_sample = 100
+    for analysis in analyses:
         
-    for sample in xrange(starting_sample + 1, starting_sample + analysis[1], increment):
-        current_sample = 'NA'+(str(sample).zfill(5))
-        increment = max(1,min(increment, starting_sample + analysis[1] - sample))
-        
-        if isSampleDone(current_sample, global_upload_state):
-            continue
-                
-        if global_upload_state is False: 
-            # We download the data from Highlander
-            #if saveForLater(cur, current_sample, analysis[0]):
-            if saveToTSV(cur, sample, sample+increment, analysis[0]):
-                addSampleDone(current_sample, False)
-            else:
-                break
-                continue
-        elif isSampleDone(current_sample, False):
-            # If we are in the upload state, we upload the data if it was previously downloaded
-            print(current_sample+": Uploading data."),
-            st = time.time()
-            if uploadToHbase(current_sample, analysis[0]):
-                addSampleDone(current_sample)
-                print("Time: "+str(round(time.time()-st,2))+"s.")
-            else:
-                print("Time: "+str(round(time.time()-st,2))+"s.")
-                print(current_sample+": Uploading data -> Failed. ")
+        # For each sample we will download the data, then create a json from it, and upload it to hbase
+        if global_upload_state is False:
+            increment = 5000
         else:
-            print(current_sample+": variants not previously downloaded.")
-            continue
+            increment = 1
+            
+        for sample in xrange(starting_sample + 1, starting_sample + analysis[1], increment):
+            current_sample = 'NA'+(str(sample).zfill(5))
+            increment = max(1,min(increment, starting_sample + analysis[1] - sample))
+            
+            if isSampleDone(current_sample, global_upload_state):
+                continue
+                    
+            if global_upload_state is False: 
+                # We download the data from Highlander
+                #if saveForLater(cur, current_sample, analysis[0]):
+                if saveToTSV(cur, sample, sample+increment, analysis[0]):
+                    addSampleDone(current_sample, False)
+                else:
+                    break
+                    continue
+            elif isSampleDone(current_sample, False):
+                # If we are in the upload state, we upload the data if it was previously downloaded
+                print(current_sample+": Uploading data."),
+                st = time.time()
+                if uploadToHbase(current_sample, analysis[0]):
+                    addSampleDone(current_sample)
+                    print("Time: "+str(round(time.time()-st,2))+"s.")
+                else:
+                    print("Time: "+str(round(time.time()-st,2))+"s.")
+                    print(current_sample+": Uploading data -> Failed. ")
+            else:
+                print(current_sample+": variants not previously downloaded.")
+                continue
 
-    starting_sample += analysis[1] + 1
+        starting_sample += analysis[1] + 1
 
-print("The end!")
-                              
-# We close the connexion
-highlander_connexion.close()                            
+    print("The end!")
+                                  
+    # We close the connexion
+    #highlander_connexion.close()                            
